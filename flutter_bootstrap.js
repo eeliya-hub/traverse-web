a0648d9cd725d7fad7c2401a38f92677829d8126
eeliya <eeliya.nayeri@gmail.com>
2026-03-24 18:43:49 +0000

    Fix base-href for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "226747683"
+    serviceWorkerVersion: "3798027052"
   }
 });
